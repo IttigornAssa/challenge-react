5a5aa2ab6560275c574cf387e480667a37d50232
ittik <boat_z_555@hotmail.com>
2024-03-05 19:52:20 +0700

    develop ui card

--- src/App.js
+++ src/App.js
@@ -5,8 +5,54 @@ import { connect } from 'react-redux';
 import { summaryDonations } from './helpers';
 
 const Card = styled.div`
-  margin: 10px;
   border: 1px solid #ccc;
+  width: 37%;
+  display: inline-block;
+  margin-left: 1.5em;
+  margin-bottom: 2em;
+  margin-right: 1.25em;
+  border-radius: 5px;
+  border: none;
+  box-shadow: #ececec 0px 7px 10px 0px;
+`;
+
+const EmptyCard = styled.div`
+  width: 40%;
+  margin-bottom: 2em;
+  margin-right: 1.25em;
+  display: inline-block;
+`;
+
+const Container = styled.div`
+  width: 80%;
+  margin: auto;
+  text-align: center;
+`;
+
+const DonateDivName = styled.div`
+  width: 70%;
+  display: inline-block;
+  text-align: left;
+  text-indent: 2em;
+  padding-top: 1rem; 
+  font-weight: bold;
+`;
+
+const DonateDivButton = styled.div`
+  width: 30%;
+  display: inline-block;
+`;
+
+const DonateButton = styled.button`
+  color: #0d6efd;
+  border-color: #0d6efd;
+  font-size: 1em;
+  font-weight: bold;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #0d6efd;
+  border-radius: 3px;
+  background: white;
 `;
 
 export default connect((state) => state)(
@@ -40,6 +86,8 @@ export default connect((state) => state)(
 
     render() {
       const self = this;
+      const count = this.state.charities.length;
+      console.log(count);
       const cards = this.state.charities.map(function (item, i) {
         const payments = [10, 20, 50, 100, 500].map((amount, j) => (
           <label key={j}>
@@ -54,10 +102,19 @@ export default connect((state) => state)(
           </label>
         ));
 
-        return (
+        const imageStyle = {
+          width: '100%',
+          height: '250px',
+        };
+        const index = i + 1;
+        return count != index ? (
           <Card key={i}>
-            <p>{item.name}</p>
-            {payments}
+            <img src={'/images/' + item.image} style={imageStyle}></img>
+            <DonateDivName>{item.name}</DonateDivName>
+            <DonateDivButton>
+              <DonateButton>Donate</DonateButton>
+            </DonateDivButton>
+            {/* {payments}
             <button
               onClick={handlePay.call(
                 self,
@@ -67,8 +124,32 @@ export default connect((state) => state)(
               )}
             >
               Pay
-            </button>
+            </button> */}
           </Card>
+        ) : count % 2 != 0 ? (
+          <>
+            <Card key={i}>
+              <img src={'/images/' + item.image} style={imageStyle}></img>
+              <DonateDivName>{item.name}</DonateDivName>
+              <DonateDivButton>
+                <DonateButton>Donate</DonateButton>
+              </DonateDivButton>
+              {/* {payments}
+              <button
+                onClick={handlePay.call(
+                  self,
+                  item.id,
+                  self.state.selectedAmount,
+                  item.currency
+                )}
+              >
+                Pay
+              </button> */}
+            </Card>
+            <EmptyCard key={i}></EmptyCard>
+          </>
+        ) : (
+          ''
         );
       });
 
@@ -84,12 +165,14 @@ export default connect((state) => state)(
       const message = this.props.message;
 
       return (
-        <div>
+        <Container>
           <h1>Tamboon React</h1>
           <p>All donations: {donate}</p>
           <p style={style}>{message}</p>
+          {/* <div style={{display:'flex'}}> */}
           {cards}
-        </div>
+          {/* </div> */}
+        </Container>
       );
     }
   }
--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,25 @@ const config = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
-
-  devtool: 'inline-source-map',
-
   devServer: {
-    inline: true,
-    host: '0.0.0.0',
+    open: true,
+    host: 'localhost',
     port: 3000,
     historyApiFallback: true,
     disableHostCheck: true,
     contentBase: 'public',
   },
+
+  devtool: 'inline-source-map',
+
+  // devServer: {
+  //   inline: true,
+  //   host: '172.29.192.1',
+  //   port: 3000,
+  //   historyApiFallback: true,
+  //   disableHostCheck: true,
+  //   contentBase: 'public',
+  // },
 
   module: {
     rules: [
@@ -27,6 +35,14 @@ const config = {
         use: {
           loader: 'babel-loader',
         },
+      },
+      // {
+      //   test: /\.(js|jsx)$/i,
+      //   loader: 'babel-loader',
+      // },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
       },
     ],
   },
